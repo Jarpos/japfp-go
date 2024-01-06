2da8c226451b0765a981b855cac2aae4f43ced31
Jarpos <51173894+Jarpos@users.noreply.github.com>
2024-01-06 18:19:43 +0100

    ref: change WritePixel to use color.Color

--- communication/pixel.go
+++ communication/pixel.go
@@ -2,6 +2,7 @@ package communication
 
 import (
 	"fmt"
+	"image/color"
 	"net"
 )
 
@@ -17,6 +18,20 @@ func ReadPixel(connection net.Conn, x int, y int) error {
 	return nil
 }
 
-func WritePixel(connection net.Conn, x int, y int, color uint32) {
-	write(connection, fmt.Sprintf("PX %d %d %06x\n", x, y, color))
+func WritePixel(connection net.Conn, x int, y int, color color.Color) {
+	write(connection, fmt.Sprintf("PX %d %d %06x\n", x, y, makeColor(color)))
 }
+
+func makeColor(color color.Color) uint32 {
+	r, g, b, _ := color.RGBA()
+	return (uint32(uint8(r)) << 16) | (uint32(uint8(g)) << 8) | uint32(uint8(b))
+}
+
+// func WritePixel(connection net.Conn, x int, y int, color color.Color) {
+// 	write(connection, fmt.Sprintf("PX %d %d %08x\n", x, y, makeColor(color)))
+// }
+//
+// func makeColor(color color.Color) uint32 {
+// 	r, g, b, a := color.RGBA()
+// 	return (uint32(r) << 24) | (uint32(g) << 16) | (uint32(b) << 8) | (uint32(a))
+// }
