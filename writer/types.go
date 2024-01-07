985df3be5df98348c67dc3288d84a27c97db6edc
Jarpos <51173894+Jarpos@users.noreply.github.com>
2024-01-07 20:25:07 +0100

    ref: add some types that i might use for the writers

---
+++ writer/types.go
@@ -0,0 +1,19 @@
+package writer
+
+import (
+	"Jarpos/japfp-go/communication"
+	"image"
+)
+
+type Writer interface {
+	Write(communication.Server, image.Image)
+}
+
+type rect struct {
+	X int
+	Y int
+}
+
+func crect(x int, y int) rect {
+	return rect{X: x, Y: y}
+}
