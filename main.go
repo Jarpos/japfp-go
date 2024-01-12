6ea27268414679842e2f771a560e3bafccfc91eb
Jarpos <51173894+Jarpos@users.noreply.github.com>
2024-01-12 01:00:54 +0100

    ref: add help info, use writer.FUNCTIONS array, add logfile output

--- main.go
+++ main.go
@@ -14,9 +14,14 @@ import (
 )
 
 func main() {
-	start := time.Now()
-	server := communication.CreateServer(127, 0, 0, 1, 1337)
+	if len(os.Args) < 3 {
+		printInfo()
+		return
+	}
 
+	start := time.Now()
+
+	server := communication.CreateServer(127, 0, 0, 1, 1337)
 	err := server.Connect()
 	if err != nil {
 		panic(err)
@@ -31,11 +36,11 @@ func main() {
 		panic(err)
 	}
 
-	writer.ChanneledTiling(server, img)
-	// writer.WriteTiling(server, img)
+	writer.FUNCTIONS[os.Args[2]].Writer(server, img)
 
 	elapsed := time.Since(start)
 	fmt.Printf("Time taken: %dms (roughly)\n", elapsed.Milliseconds())
+	logTimings(&server, elapsed)
 }
 
 func readImage(path string) (image.Image, error) {
@@ -47,4 +52,19 @@ func readImage(path string) (image.Image, error) {
 
 	image, _, err := image.Decode(f)
 	return image, err
+}
+
+func printInfo() {
+	fmt.Printf("%s [PICTURE] [WRITER]\n\n", os.Args[0])
+	fmt.Printf("Writers are:\n")
+	for k, v := range writer.FUNCTIONS {
+		fmt.Printf("%s: %s\n", k, v.Help)
+	}
+	fmt.Printf("\n")
+}
+
+func logTimings(server *communication.Server, elapsed time.Duration) {
+	f, _ := os.OpenFile("timings.log", (os.O_APPEND | os.O_CREATE | os.O_WRONLY), 0644)
+	defer f.Close()
+	fmt.Fprintf(f, "%d\t%d\t%dms\n", server.SizeX, server.SizeY, elapsed.Milliseconds())
 }
