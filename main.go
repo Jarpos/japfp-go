121cc88bf2fec9aeb11cf88b2e4591aa5eb22d41
Jarpos <51173894+Jarpos@users.noreply.github.com>
2024-01-15 00:02:01 +0100

    ref: change args to be proper cmdline args using the flag-stdlib

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"Jarpos/japfp-go/communication"
 	"Jarpos/japfp-go/writer"
 
+	"flag"
 	"fmt"
 	"net"
 	"os"
@@ -14,15 +15,19 @@ import (
 	_ "image/png"
 )
 
-func main() {
-	if len(os.Args) < 3 {
-		printInfo()
-		return
-	}
+type Settings struct {
+	Writer    string
+	ImagePath string
+	Server    string
+	Port      uint
+}
 
+func main() {
 	start := time.Now()
 
-	server := communication.CreateServer(net.IPv4(127, 0, 0, 1), 1337)
+	settings := parseArgs()
+
+	server := communication.CreateServer(net.ParseIP(settings.Server), uint16(settings.Port))
 	err := server.Connect()
 	if err != nil {
 		panic(err)
@@ -32,16 +37,16 @@ func main() {
 	fmt.Printf("Connection to %s established\n", server.Host.String())
 	fmt.Printf("Canvas size %dx%d (%d pixels)\n", server.SizeX, server.SizeY, server.SizeX*server.SizeY)
 
-	img, err := readImage(os.Args[1])
+	img, err := readImage(settings.ImagePath)
 	if err != nil {
 		panic(err)
 	}
 
-	writer.FUNCTIONS[os.Args[2]].Writer(server, img)
+	writer.FUNCTIONS[settings.Writer].Writer(server, img)
 
 	elapsed := time.Since(start)
 	fmt.Printf("Time taken: %dms (roughly)\n", elapsed.Milliseconds())
-	logTimings(&server, elapsed)
+	logTimings(&server, elapsed, settings.Writer)
 }
 
 func readImage(path string) (image.Image, error) {
@@ -55,17 +60,31 @@ func readImage(path string) (image.Image, error) {
 	return image, err
 }
 
-func printInfo() {
-	fmt.Printf("%s [PICTURE] [WRITER]\n\n", os.Args[0])
-	fmt.Printf("Writers are:\n")
-	for k, v := range writer.FUNCTIONS {
-		fmt.Printf("%s: %s\n", k, v.Help)
+func parseArgs() Settings {
+	settings := Settings{}
+	flag.StringVar(&settings.Writer /*****/, "w", "ct", "Writer to choose")
+	flag.StringVar(&settings.ImagePath /**/, "i", "pictures/ConveyorBelt.png", "Image to use")
+	flag.StringVar(&settings.Server /*****/, "s", "127.0.0.1", "Server to connect to")
+	flag.UintVar(&settings.Port /*********/, "p", 1337, "Port to connect to")
+
+	help := flag.Bool("help", false, "Print help information")
+	flag.Parse()
+
+	if *help {
+		flag.Usage()
+		fmt.Printf("\nWriters are:\n")
+		for k, v := range writer.FUNCTIONS {
+			fmt.Printf("%s: %s\n", k, v.Help)
+		}
+		fmt.Printf("\n")
+		os.Exit(0)
 	}
-	fmt.Printf("\n")
+
+	return settings
 }
 
-func logTimings(server *communication.Server, elapsed time.Duration) {
+func logTimings(server *communication.Server, elapsed time.Duration, writerStr string) {
 	f, _ := os.OpenFile("timings.log", (os.O_APPEND | os.O_CREATE | os.O_WRONLY), 0644)
 	defer f.Close()
-	fmt.Fprintf(f, "%d\t%d\t%dms\t%s\n", server.SizeX, server.SizeY, elapsed.Milliseconds(), os.Args[2])
+	fmt.Fprintf(f, "%d\t%d\t%dms\t%s\n", server.SizeX, server.SizeY, elapsed.Milliseconds(), writerStr)
 }
